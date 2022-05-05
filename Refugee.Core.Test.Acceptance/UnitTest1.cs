f9177d05ee5b890f21de35040f51c5028feb7dc7
ahmed magdi <ahmed.magdi@bgomla.com>
2022-05-05 17:34:07 +0200

    INFRA: Initialize Acceptance Test Project

---
+++ Refugee.Core.Test.Acceptance/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Refugee.Core.Test.Acceptance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
