f55b281a921817c77fcd84c6cc5e305858cb311c
ElbekDeveloper <e.normurodov@mail.ru>
2022-05-04 21:45:24 -0700

    CODE RUB: Clean Files

--- Refugee.Core.Api/Controllers/HomeController.cs
+++ Refugee.Core.Api/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using RESTFulSense.Controllers;
 
 namespace Refugee.Core.Api.Controllers
 {
-    
+
     [ApiController]
     [Route("api/[controller]")]
     public class HomeController : RESTFulController
