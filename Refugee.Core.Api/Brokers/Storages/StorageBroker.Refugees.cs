41f468fc667ebaedf1e9d5c1be8c9fd3df8d5fe2
hoxhaleo319 <burim@piorsoft.com>
2022-05-06 05:25:33 -0700

    DATA: Refugee Model

---
+++ Refugee.Core.Api/Brokers/Storages/StorageBroker.Refugees.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using Refugee.Core.Api.Models.Refugees;
+
+namespace Refugee.Core.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+            // Error CS0118
+            // 'Refugee' is a namespace but is used like a type    
+
+            // public DbSet<Refugee> Refugees { get; set; }
+            // Todo:Ask Hassan "Do I have to wait for the storage broker to be setup?"
+           
+
+    }
+}
---
+++ Refugee.Core.Api/Models/Refugees/Refugee.cs
@@ -0,0 +1,34 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Refugee.Core.Api.Models.Refugees
+{
+    public class Refugee
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string CurrentLocation { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string AdditionalDetails { get; set; }
+        public IList<string> Languages { get; set; }
+        public IList<string> Nationalities { get; set; }
+        public IList<string> FamilyMembers { get; set; }
+        public IList<string> Pets { get; set; }
+        public IList<string> Properties { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public GenderType Gender { get; set; }
+
+    }
+
+    public enum GenderType 
+    { MALE = 0, 
+      FEMALE = 1, 
+      OTHER = 2,
+    }
+}
