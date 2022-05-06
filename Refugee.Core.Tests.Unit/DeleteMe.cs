8183123a62745569e445e905a73538f2491b05b2
hoxhaleo319 <burim@piorsoft.com>
2022-05-05 23:14:39 -0700

    ShouldReturnTrue -> FAIL

---
+++ Refugee.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Refugee.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldReturnTrue()
+        {
+            Assert.True(false); 
+        }
+    }
+}
