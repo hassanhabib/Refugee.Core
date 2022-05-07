d613665330c1e588c461b57822bac5594db50e46
hoxhaleo319 <burim@piorsoft.com>
2022-05-06 20:51:16 -0700

    INFRA: Initialize Unit Tests Project

---
+++ Refugee.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using Xunit;
+
+namespace Refugee.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldReturnTrue() => Assert.True(true);
+    }
+}
