5e2f06fd6129a5b4bc2101167e5293263c6b6835
Spectralgo <florian.renard@spectralgo.com>
2022-05-09 20:29:08 +0400

    CONTROLLER: Add Post Refugee

---
+++ RefugeeLand.Core.Api/Controllers/RefugeesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RefugeeLand.Core.Api.Models.Refugees;
+using RefugeeLand.Core.Api.Services.Foundations.Refugees;
+using RESTFulSense.Controllers;
+
+namespace RefugeeLand.Core.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RefugeesController : RESTFulController
+{
+    private readonly IRefugeeService refugeeService;
+
+    public RefugeesController(IRefugeeService refugeeService)
+    {
+        this.refugeeService = refugeeService;
+    }
+
+    [HttpPost]
+    public async ValueTask<ActionResult<Refugee>> PostRefugeeAsync(Refugee refugee)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ RefugeeLand.Core.Api/Services/Foundations/Refugees/IRefugeeService.cs
@@ -0,0 +1,10 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+namespace RefugeeLand.Core.Api.Services.Foundations.Refugees
+{
+    public interface IRefugeeService
+    { }
+}
