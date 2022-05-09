04f1b158c9285f7f59b17da0ff13cf805401bb79
Spectralgo <florian.renard@spectralgo.com>
2022-05-09 14:23:01 +0400

    BROKERS: Add DateTime

---
+++ RefugeeLand.Core.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+
+namespace RefugeeLand.Core.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
---
+++ RefugeeLand.Core.Api/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+
+namespace RefugeeLand.Core.Api.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTimeOffset();
+    }
+}
