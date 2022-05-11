46b98b8b79dab5d5f4caada2eba8eb8b540399f6
Spectralgo <florian.renard@spectralgo.com>
2022-05-11 08:29:58 +0400

    DATA: Add Refugee Migration

---
+++ RefugeeLand.Core.Api/Migrations/20220511041203_AddRefugeeMigrationAddMiddleName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefugeeLand.Core.Api.Migrations
+{
+    public partial class AddRefugeeMigrationAddMiddleName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Refugees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Refugees");
+        }
+    }
+}
--- RefugeeLand.Core.Api/Models/Refugees/Refugee.cs
+++ RefugeeLand.Core.Api/Models/Refugees/Refugee.cs
@@ -4,7 +4,6 @@
 // -------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using RefugeeLand.Core.Api.Models.Enums;
 
 namespace RefugeeLand.Core.Api.Models.Refugees
@@ -13,20 +12,21 @@ namespace RefugeeLand.Core.Api.Models.Refugees
     {
         public Guid Id { get; set; }
         public string FirstName { get; set; }
+        public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string CurrentLocation { get; set; }
         public Gender Gender { get; set; }
         public DateTimeOffset BirthDate { get; set; }
-        public IEnumerable<string> MedicalConditions { get; set; }
-        public IEnumerable<string> SkillSets { get; set; }
-        public IEnumerable<string> Languages { get; set; }
-        public bool IsOpenToWork { get; set; }
-        public string AdditionalDetails { get; set; }
         //todo: Replace with Contact information class
         public string Phone { get; set; }
         public string Email { get; set; }
         //todo: Add Family class or RefugeeGroup
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
+        public bool IsOpenToWork { get; set; }
+        public string MedicalConditions { get; set; }
+        public string SkillSets { get; set; }
+        public string Languages { get; set; }
+        public string AdditionalDetails { get; set; }
     }
 }
