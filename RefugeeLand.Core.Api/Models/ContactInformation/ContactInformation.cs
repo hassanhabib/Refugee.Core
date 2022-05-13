911045c9b6542065684f169fa875892a70bb20e7
Bhavesh Ramburn <nitrogen@gmail.com>
2022-05-13 16:14:50 +0100

    Added ContactInformation.cs model

---
+++ RefugeeLand.Core.Api/Models/ContactInformation/ContactInformation.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+
+using System;
+
+namespace RefugeeLand.Core.Api.Models.ContactInformation;
+
+// can be reused
+public class ContactInformation
+{
+    public Guid Guid { get; set; }
+    // full qualified ISO E.123 phone number
+    public string Phone { get; set; }
+    public string Email { get; set; }
+}
