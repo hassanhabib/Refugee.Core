3aa86863fe0dda24df475d9eccd9e4ee150dfa2c
Ricky <rcruzfreelance@outlook.com>
2022-05-19 21:58:52 -0400

    PROCESSINGS: Adding Cloud Management Processing Service

---
+++ RefugeeLand.Core.Api.Infrastructure.Provision/Services/Processings/ICloudManagementProcessingService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace RefugeeLand.Core.Api.Infrastructure.Provision.Services.Processings
+{
+    public interface ICloudManagementProcessingService
+    {
+        ValueTask ProcessAsync();
+    }
+}
