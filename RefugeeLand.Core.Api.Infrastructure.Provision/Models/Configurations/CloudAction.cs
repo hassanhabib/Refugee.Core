0334d22de86b84990f3ce32b3495478e5b458134
Ricky <rcruzfreelance@outlook.com>
2022-05-19 21:23:22 -0400

    DATA: Added models for sotrages and configurations

---
+++ RefugeeLand.Core.Api.Infrastructure.Provision/Models/Configurations/CloudAction.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace RefugeeLand.Core.Api.Infrastructure.Provision.Models.Configurations
+{
+    public class CloudAction
+    {
+        public List<string> Environments { get; set; }
+    }
+}
---
+++ RefugeeLand.Core.Api.Infrastructure.Provision/Models/Storages/SqlDatabase.cs
@@ -0,0 +1,8 @@
+﻿namespace RefugeeLand.Core.Api.Infrastructure.Provision.Models.Storages
+{
+    public class SqlDatabase
+    {
+        public string AdminName { get; set; }
+        public string AdminAccess { get; set; }
+    }
+}
