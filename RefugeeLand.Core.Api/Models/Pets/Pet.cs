4c595a1805aa4006b933f50bdec86c6400e6da65
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-13 15:20:20 +0400

    DATA: Add Pet Model

---
+++ RefugeeLand.Core.Api/Models/Pets/Pet.cs
@@ -0,0 +1,25 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+
+namespace RefugeeLand.Core.Api.Models.Pets
+{
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public PetType Type { get; set; }
+        public PetGender Gender { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public string AdditionalDetails { get; set; }
+        
+        // [JsonIgnore]
+        // public IEnumerable<PetMedicalCondition> PetMedicalConditions { get; set; }
+        
+        // [JsonIgnore]
+        // public IEnumerable<RefugeePet> RefugeePets { get; set; }
+    }
+}
