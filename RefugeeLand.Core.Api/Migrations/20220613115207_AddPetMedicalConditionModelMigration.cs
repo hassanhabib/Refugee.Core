0885c048479486e6091ccfce49413d215deeef71
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-13 15:54:11 +0400

    DATA: Add Pet Medical Condition Migration

---
+++ RefugeeLand.Core.Api/Migrations/20220613115207_AddPetMedicalConditionModelMigration.cs
@@ -0,0 +1,51 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefugeeLand.Core.Api.Migrations
+{
+    public partial class AddPetMedicalConditionModelMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetMedicalConditions",
+                columns: table => new
+                {
+                    MedicalConditionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetMedicalConditions", x => new { x.PetId, x.MedicalConditionId });
+                    table.ForeignKey(
+                        name: "FK_PetMedicalConditions_MedicalConditions_MedicalConditionId",
+                        column: x => x.MedicalConditionId,
+                        principalTable: "MedicalConditions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PetMedicalConditions_Pets_PetId",
+                        column: x => x.PetId,
+                        principalTable: "Pets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetMedicalConditions_MedicalConditionId",
+                table: "PetMedicalConditions",
+                column: "MedicalConditionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PetMedicalConditions");
+        }
+    }
+}
