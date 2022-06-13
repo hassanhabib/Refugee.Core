d6c15d2b7f3b0ed8827d2c40532e21c787370ea2
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-13 17:53:47 +0400

    DATA: Add Host Model

---
+++ RefugeeLand.Core.Api/Models/Hosts/Host.cs
@@ -0,0 +1,46 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+
+namespace RefugeeLand.Core.Api.Models.Hosts
+{
+    public class Host
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string AdditionalDetails { get; set; }
+        public HostGender Gender { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<Shelter> Shelters { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<HostContact> HostContacts { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<HostFamilyMembership> HostFamilyMemberships { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<HostLanguage> HostLanguages { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<HostNationality> HostNationalities { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<HostMedicalCondition> HostMedicalConditions { get; set; }
+
+        // [JsonIgnore] 
+        // public IEnumerable<HostDocument> HostDocuments { get; set; }
+    }
+}
