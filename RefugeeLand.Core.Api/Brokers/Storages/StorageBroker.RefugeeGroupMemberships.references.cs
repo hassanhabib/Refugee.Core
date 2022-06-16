3cd24bd455f69ffad746cbefcdad1a859ca76e4b
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-16 19:50:19 +0400

    DATA: Add RefugeeGroupMembership Model

---
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.RefugeeGroupMemberships.references.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+
+using Microsoft.EntityFrameworkCore;
+using RefugeeLand.Core.Api.Models.RefugeeGroupMemberships;
+
+namespace RefugeeLand.Core.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        private static void SetRefugeeGroupMembershipReferences(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RefugeeGroupMembership>()
+                .HasKey(refugeeGroupMembership =>
+                    new { refugeeGroupMembership.RefugeeId, refugeeGroupMembership.RefugeeGroupId });
+
+            modelBuilder.Entity<RefugeeGroupMembership>()
+                .HasOne(refugeeGroupMembership => refugeeGroupMembership.Refugee)
+                .WithMany(refugee => refugee.RefugeeGroupMemberships)
+                .HasForeignKey(refugeeGroupMembership => refugeeGroupMembership.RefugeeId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<RefugeeGroupMembership>()
+                .HasOne(refugeeGroupMembership => refugeeGroupMembership.RefugeeGroup)
+                .WithMany(refugeeGroup => refugeeGroup.RefugeeGroupMemberships)
+                .HasForeignKey(refugeeGroupMembership => refugeeGroupMembership.RefugeeGroupId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
--- RefugeeLand.Core.Api/Models/RefugeeGroups/RefugeeGroup.cs
+++ RefugeeLand.Core.Api/Models/RefugeeGroups/RefugeeGroup.cs
@@ -5,15 +5,29 @@
 
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using RefugeeLand.Core.Api.Models.RefugeeGroupMemberships;
 using RefugeeLand.Core.Api.Models.Refugees;
 
 namespace RefugeeLand.Core.Api.Models.RefugeeGroups;
 
 public class RefugeeGroup
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public IList<Refugee> Refugee { get; set; }
-    public DateTimeOffset CreatedDate { get; set; }
-    public DateTimeOffset UpdatedDate { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+		//One who speaks for the group during negotiations and requests
+        public Guid RefugeeGroupMainRepresentativeId { get; set; }
+        public Refugee RefugeeGroupMainRepresentative { get; set; }
+        
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+            
+        [JsonIgnore]
+        public IEnumerable<RefugeeGroupMembership> RefugeeGroupMemberships { get; set; }
+
+        // [JsonIgnore]
+        // public IEnumerable<ShelterRequest> ShelterRequests { get; set; }
 }
