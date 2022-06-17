354dd1e01dd846e6d8ab1d75a77019b2c8849f82
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-17 15:18:15 +0400

    DATA: Update AllowedPet Migration

---
+++ RefugeeLand.Core.Api/Migrations/20220617111456_AddAllowedPetMigration.cs
@@ -0,0 +1,78 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefugeeLand.Core.Api.Migrations
+{
+    public partial class AddAllowedPetMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AllowedPet_Shelters_ShelterId",
+                table: "AllowedPet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AllowedPet",
+                table: "AllowedPet");
+
+            migrationBuilder.RenameTable(
+                name: "AllowedPet",
+                newName: "AllowedPets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AllowedPet_ShelterId",
+                table: "AllowedPets",
+                newName: "IX_AllowedPets_ShelterId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AllowedPets",
+                table: "AllowedPets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AllowedPets_Shelters_ShelterId",
+                table: "AllowedPets",
+                column: "ShelterId",
+                principalTable: "Shelters",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AllowedPets_Shelters_ShelterId",
+                table: "AllowedPets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AllowedPets",
+                table: "AllowedPets");
+
+            migrationBuilder.RenameTable(
+                name: "AllowedPets",
+                newName: "AllowedPet");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AllowedPets_ShelterId",
+                table: "AllowedPet",
+                newName: "IX_AllowedPet_ShelterId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AllowedPet",
+                table: "AllowedPet",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AllowedPet_Shelters_ShelterId",
+                table: "AllowedPet",
+                column: "ShelterId",
+                principalTable: "Shelters",
+                principalColumn: "Id");
+        }
+    }
+}
