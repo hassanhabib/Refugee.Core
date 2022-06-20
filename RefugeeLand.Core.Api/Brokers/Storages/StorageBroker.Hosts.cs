680337a27d36a8f4adfb0779200b507164266609
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-20 17:13:23 +0400

    BROKERS: Insert Host

--- RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Hosts.cs
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Hosts.cs
@@ -3,7 +3,11 @@
 // FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
 // -------------------------------------------------------
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using RefugeeLand.Core.Api.Models.Hosts;
 
 namespace RefugeeLand.Core.Api.Brokers.Storages
@@ -11,5 +15,17 @@ namespace RefugeeLand.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Host> Hosts { get; set; }
+        
+        public async ValueTask<Host> InsertHostAsync(Host host)
+        {
+            using var broker = new StorageBroker(configuration);
+
+            EntityEntry<Host> hostEntityEntry = 
+                await broker.AddAsync(host);
+
+            await broker.SaveChangesAsync();
+
+            return hostEntityEntry.Entity;
+        } 
     }
 }
