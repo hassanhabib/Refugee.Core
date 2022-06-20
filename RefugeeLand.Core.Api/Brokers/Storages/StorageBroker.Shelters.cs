8933167f592acdcce619cbb4c4e19d80b4a24d7b
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-06-20 20:27:33 +0400

    BROKERS: Insert Shelter

--- RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Shelters.cs
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Shelters.cs
@@ -4,7 +4,9 @@
 // -------------------------------------------------------
 
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using RefugeeLand.Core.Api.Models.Shelters;
 
 namespace RefugeeLand.Core.Api.Brokers.Storages
@@ -12,5 +14,17 @@ namespace RefugeeLand.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Shelter> Shelters { get; set; }
+
+        public async ValueTask<Shelter> InsertShelterAsync(Shelter shelter)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Shelter> shelterEntityEntry =
+                await broker.AddAsync(shelter);
+
+            await broker.SaveChangesAsync();
+
+            return shelterEntityEntry.Entity;
+        }
     }
 }
