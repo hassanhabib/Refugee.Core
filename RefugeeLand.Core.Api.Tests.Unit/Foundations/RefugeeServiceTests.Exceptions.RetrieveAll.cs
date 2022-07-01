7ff1b4bb665168ffb0f1f705e48ccf6701c9a25e
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-07-01 05:30:24 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Foundations/RefugeeServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using RefugeeLand.Core.Api.Models.Refugees.Exceptions;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Foundations
+{
+    public partial class RefugeeServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedRefugeeStorageException =
+                new FailedRefugeeStorageException(sqlException);
+
+            var expectedRefugeeDependencyException =
+                new RefugeeDependencyException(failedRefugeeStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllRefugees())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllRefugeeAction = () =>
+                this.refugeeService.RetrieveAllRefugees();
+            
+            RefugeeDependencyException actualRefugeeDependencyException =
+                Assert.Throws<RefugeeDependencyException>(
+                    retrieveAllRefugeeAction);
+
+            // then
+            actualRefugeeDependencyException.Should()
+                .BeEquivalentTo(expectedRefugeeDependencyException);
+            
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllRefugees(),
+                    Times.Once);
+            
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedRefugeeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            
+        }
+    }
+}
