3a383d8aff3a44dbfb194bb294842841000d3695
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-07-02 17:24:20 +0400

    BROKERS: Insert RefugeeGroup

--- RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.RefugeeGroups.cs
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.RefugeeGroups.cs
@@ -3,7 +3,9 @@
 // FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
 // -------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using RefugeeLand.Core.Api.Models.RefugeeGroups;
 
 namespace RefugeeLand.Core.Api.Brokers.Storages
@@ -11,5 +13,17 @@ namespace RefugeeLand.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<RefugeeGroup> RefugeeGroups { get; set; }
+
+        public async ValueTask<RefugeeGroup> InsertRefugeeGroupAsync(RefugeeGroup refugeeGroup)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<RefugeeGroup> refugeeGroupEntityEntry =
+                await broker.RefugeeGroups.AddAsync(refugeeGroup);
+
+            await broker.SaveChangesAsync();
+
+            return refugeeGroupEntityEntry.Entity;
+        }
     }
 }
