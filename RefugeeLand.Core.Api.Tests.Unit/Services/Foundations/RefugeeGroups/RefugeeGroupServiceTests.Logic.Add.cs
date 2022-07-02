5ed537c22ffa311b3f2efb34bca4cac5b738993d
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-07-02 18:27:41 +0400

    ShouldAddRefugeeGroupAsync -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/RefugeeGroups/RefugeeGroupServiceTests.Logic.Add.cs
@@ -0,0 +1,54 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using RefugeeLand.Core.Api.Models.RefugeeGroups;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.RefugeeGroups
+{
+    public partial class RefugeeGroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddRefugeeGroupAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            RefugeeGroup randomRefugeeGroup = CreateRandomRefugeeGroup(randomDateTime);
+            RefugeeGroup inputRefugeeGroup = randomRefugeeGroup;
+            RefugeeGroup insertedRefugeeGroup = inputRefugeeGroup;
+            RefugeeGroup expectedRefugeeGroup = insertedRefugeeGroup.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertRefugeeGroupAsync(inputRefugeeGroup))
+                    .ReturnsAsync(insertedRefugeeGroup);
+
+            // when
+            RefugeeGroup actualRefugeeGroup =
+                await this.refugeeGroupService.AddRefugeeGroupAsync(inputRefugeeGroup);
+
+            // then
+            actualRefugeeGroup.Should().BeEquivalentTo(expectedRefugeeGroup);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertRefugeeGroupAsync(inputRefugeeGroup),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/RefugeeGroups/RefugeeGroupServiceTests.cs
@@ -0,0 +1,58 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using Moq;
+using RefugeeLand.Core.Api.Brokers.DateTimes;
+using RefugeeLand.Core.Api.Brokers.Loggings;
+using RefugeeLand.Core.Api.Brokers.Storages;
+using RefugeeLand.Core.Api.Models.RefugeeGroups;
+using RefugeeLand.Core.Api.Services.Foundations.RefugeeGroups;
+using Tynamix.ObjectFiller;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.RefugeeGroups
+{
+    public partial class RefugeeGroupServiceTests
+    {
+    
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IRefugeeGroupService refugeeGroupService;
+
+        public RefugeeGroupServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.refugeeGroupService = new RefugeeGroupService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+        
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+        
+        private static RefugeeGroup CreateRandomRefugeeGroup() =>
+            CreateRefugeeGroupFiller(dates: DateTimeOffset.UtcNow).Create();
+
+        private static RefugeeGroup CreateRandomRefugeeGroup(DateTimeOffset dates) =>
+            CreateRefugeeGroupFiller(dates).Create();
+        
+        private static Filler<RefugeeGroup> CreateRefugeeGroupFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<RefugeeGroup>();
+
+            filler.Setup()
+                .OnProperty(refugeeGroup => refugeeGroup.ShelterRequests).IgnoreIt()
+                .OnProperty(refugeeGroup => refugeeGroup.RefugeeGroupMemberships).IgnoreIt()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+
+}
