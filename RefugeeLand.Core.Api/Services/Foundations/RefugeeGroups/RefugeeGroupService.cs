03d653c211725c984cb59a4a8f2331c7ed96df7a
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-07-02 17:51:19 +0400

    WIP

---
+++ RefugeeLand.Core.Api/Services/Foundations/RefugeeGroups/RefugeeGroupService.cs
@@ -0,0 +1,36 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using RefugeeLand.Core.Api.Brokers.DateTimes;
+using RefugeeLand.Core.Api.Brokers.Loggings;
+using RefugeeLand.Core.Api.Brokers.Storages;
+using RefugeeLand.Core.Api.Models.RefugeeGroups;
+
+namespace RefugeeLand.Core.Api.Services.Foundations.RefugeeGroups
+{
+    public class RefugeeGroupService : IRefugeeGroupService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public RefugeeGroupService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<RefugeeGroup> AddRefugeeGroupAsync(RefugeeGroup refugeeGroup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
