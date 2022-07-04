91e23e30696bb67889daa06c025f9aeee931073e
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-07-04 23:34:51 +0400

    ShouldThrowValidationExceptionOnAddIfRefugeeGroupIsInvalidAndLogItAsync -> FAIL

--- RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/RefugeeGroups/RefugeeGroupServiceTests.Validations.Add.cs
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/RefugeeGroups/RefugeeGroupServiceTests.Validations.Add.cs
@@ -49,5 +49,78 @@ namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.RefugeeGroups
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+        
+        
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfRefugeeGroupIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidRefugeeGroup = new RefugeeGroup
+            {
+                Name = invalidText
+            };
+
+            var invalidRefugeeGroupException =
+                new InvalidRefugeeGroupException();
+
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.Id),
+                values: "Id is required");
+
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.Name),
+                values: "Text is required");
+
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.RefugeeGroupMainRepresentativeId),
+                values: "Id is required");
+
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.CreatedBy),
+                values: "Id is required");
+
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.UpdatedBy),
+                values: "Id is required");
+            
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.CreatedDate),
+                values: "Date is required");
+            
+            invalidRefugeeGroupException.AddData(
+                key: nameof(RefugeeGroup.UpdatedDate),
+                values: "Date is required");
+
+            var expectedRefugeeGroupValidationException =
+                new RefugeeGroupValidationException(invalidRefugeeGroupException);
+            
+            //when
+            ValueTask<RefugeeGroup> addRefugeeGroupTask =
+                this.refugeeGroupService.AddRefugeeGroupAsync(invalidRefugeeGroup);
+            
+            RefugeeGroupValidationException actualRefugeeGroupValidationException =
+                await Assert.ThrowsAsync<RefugeeGroupValidationException>(
+                    addRefugeeGroupTask.AsTask);
+            
+            // then
+            actualRefugeeGroupValidationException.Should().BeEquivalentTo(
+                expectedRefugeeGroupValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedRefugeeGroupValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertRefugeeGroupAsync(It.IsAny<RefugeeGroup>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ RefugeeLand.Core.Api/Models/RefugeeGroups/Exceptions/InvalidRefugeeGroupException.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using Xeptions;
+
+namespace RefugeeLand.Core.Api.Models.RefugeeGroups.Exceptions
+{
+    public class InvalidRefugeeGroupException : Xeption
+    {
+        public InvalidRefugeeGroupException() 
+            : base(message:"Invalid RefugeeGroup. Please correct the errors and try again.")
+        { }
+    }
+}
