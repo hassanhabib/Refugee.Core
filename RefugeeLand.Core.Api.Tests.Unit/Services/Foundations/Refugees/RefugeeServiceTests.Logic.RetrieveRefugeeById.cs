2c276b3a2c18cd1eca1dcba1f700f166a693622b
TylerDDev <tyler.dang.23@gmail.com>
2022-07-17 22:13:21 -0700

    ShouldRetrieveRefugeeById -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Refugees/RefugeeServiceTests.Logic.RetrieveRefugeeById.cs
@@ -0,0 +1,44 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using RefugeeLand.Core.Api.Models.Refugees;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Refugees
+{
+    public partial class RefugeeServiceTests
+    {
+        [Fact]
+        public async void ShouldRetrieveRefugeeById()
+        {
+            //given
+            Refugee someRefugee = CreateRandomRefugee();
+            Refugee storageRefugee = someRefugee;
+            Refugee expectedRefugee = storageRefugee.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectRefugeeByIdAsync(someRefugee.Id))
+                    .ReturnsAsync(storageRefugee);
+
+            //when
+            Refugee actualRefugee =
+                await this.refugeeService.RetrieveRefugeeByIdAsync(someRefugee.Id);
+
+            //then
+            actualRefugee.Should().BeEquivalentTo(expectedRefugee);
+            
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectRefugeeByIdAsync(someRefugee.Id),
+                    Times.Once);
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
