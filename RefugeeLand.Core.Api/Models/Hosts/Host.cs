3b99d9baa644da5e8bd3c200b537b31282a80046
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-01 14:25:38 +0400

    CONFIG: Add .editorconfig to solution

--- RefugeeLand.Core.Api/Models/Hosts/Host.cs
+++ RefugeeLand.Core.Api/Models/Hosts/Host.cs
@@ -14,12 +14,12 @@ namespace RefugeeLand.Core.Api.Models.Hosts
     public class Host
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string AdditionalDetails { get; set; }
-        public HostGender Gender { get; set; }
-        public DateTimeOffset BirthDate { get; set; }
+        // public string FirstName { get; set; }
+        // public string MiddleName { get; set; }
+        // public string LastName { get; set; }
+        // public string AdditionalDetails { get; set; }
+        // public HostGender Gender { get; set; }
+        // public DateTimeOffset BirthDate { get; set; }
 
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
