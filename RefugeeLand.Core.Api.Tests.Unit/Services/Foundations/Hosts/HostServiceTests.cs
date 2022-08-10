4a7242d1508673b58ac84ee98903f75554febef8
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-10 23:12:23 +0400

    ShouldAddHostAsync -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.cs
@@ -0,0 +1,52 @@
+using System;
+using Moq;
+using RefugeeLand.Core.Api.Brokers.DateTimes;
+using RefugeeLand.Core.Api.Brokers.Loggings;
+using RefugeeLand.Core.Api.Brokers.Storages;
+using RefugeeLand.Core.Api.Models.Hosts;
+using RefugeeLand.Core.Api.Services.Foundations.Hosts;
+using Tynamix.ObjectFiller;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IHostService hostService;
+
+        public HostServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.hostService = new HostService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Host CreateRandomHost(DateTimeOffset dateTimeOffset) =>
+            CreateHostFiller(dateTimeOffset).Create();
+
+        private static Filler<Host> CreateHostFiller(DateTimeOffset dateTimeOffset)
+        {
+            Guid userId = Guid.NewGuid();
+            var filler = new Filler<Host>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnProperty(host => host.CreatedByUserId).Use(userId)
+                .OnProperty(host => host.UpdatedByUserId).Use(userId)
+
+                .OnProperty(host => host.Shelters).IgnoreIt()
+                .OnProperty(host => host.ShelterOffers).IgnoreIt();            // TODO: Complete the filler setup e.g. ignore related properties etc...
+            return filler;
+        }
+    }
+}
