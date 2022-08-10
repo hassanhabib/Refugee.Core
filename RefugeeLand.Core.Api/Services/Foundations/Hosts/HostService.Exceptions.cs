b1d6be49adec3062b2e6462e01c292ab65349d46
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-10 20:46:31 +0400

    ShouldThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync -> PASS

---
+++ RefugeeLand.Core.Api/Services/Foundations/Hosts/HostService.Exceptions.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using RefugeeLand.Core.Api.Models.Hosts;
+using RefugeeLand.Core.Api.Models.Hosts.Exceptions;
+using Xeptions;
+
+namespace RefugeeLand.Core.Api.Services.Foundations.Hosts
+{
+    public partial class HostService
+    {
+        private delegate ValueTask<Host> ReturningHostFunction();
+
+        private async ValueTask<Host> TryCatch(ReturningHostFunction returningHostFunction)
+        {
+            try
+            {
+                return await returningHostFunction();
+            }
+            catch (NullHostException nullHostException)
+            {
+                throw CreateAndLogValidationException(nullHostException);
+            }
+        }
+
+        private HostValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var hostValidationException =
+                new HostValidationException(exception);
+
+            this.loggingBroker.LogError(hostValidationException);
+
+            return hostValidationException;
+        }
+    }
+}
