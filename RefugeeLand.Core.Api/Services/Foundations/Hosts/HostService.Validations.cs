017319f63c1614ed66f449c0b36fb18c9ef5162b
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-10 22:52:20 +0400

    ShouldThrowValidationExceptionOnAddIfHostIsInvalidAndLogItAsync -> PASS

--- RefugeeLand.Core.Api/Services/Foundations/Hosts/HostService.Validations.cs
+++ RefugeeLand.Core.Api/Services/Foundations/Hosts/HostService.Validations.cs
@@ -1,3 +1,4 @@
+using System;
 using RefugeeLand.Core.Api.Models.Hosts;
 using RefugeeLand.Core.Api.Models.Hosts.Exceptions;
 
@@ -8,6 +9,16 @@ namespace RefugeeLand.Core.Api.Services.Foundations.Hosts
         private void ValidateHostOnAdd(Host host)
         {
             ValidateHostIsNotNull(host);
+
+            Validate(
+                (Rule: IsInvalid(host.Id), Parameter: nameof(Host.Id)),
+
+                // TODO: Add any other required validation rules
+
+                (Rule: IsInvalid(host.CreatedDate), Parameter: nameof(Host.CreatedDate)),
+                (Rule: IsInvalid(host.CreatedByUserId), Parameter: nameof(Host.CreatedByUserId)),
+                (Rule: IsInvalid(host.UpdatedDate), Parameter: nameof(Host.UpdatedDate)),
+                (Rule: IsInvalid(host.UpdatedByUserId), Parameter: nameof(Host.UpdatedByUserId)));
         }
 
         private static void ValidateHostIsNotNull(Host host)
@@ -16,6 +27,35 @@ namespace RefugeeLand.Core.Api.Services.Foundations.Hosts
             {
                 throw new NullHostException();
             }
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidHostException = new InvalidHostException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidHostException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidHostException.ThrowIfContainsErrors();
         }
     }
 }
