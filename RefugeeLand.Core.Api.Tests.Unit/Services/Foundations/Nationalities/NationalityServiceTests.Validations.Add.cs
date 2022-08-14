19c3bfcaf542b85a775aa20536f9b2ef65a41fc0
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-14 20:30:52 +0400

    ShouldThrowValidationExceptionOnAddIfNationalityIsInvalidAndLogItAsync -> FAIL

--- RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Nationalities/NationalityServiceTests.Validations.Add.cs
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Nationalities/NationalityServiceTests.Validations.Add.cs
@@ -53,7 +53,8 @@ namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Nationalities
             // given
             var invalidNationality = new Nationality
             {
-                // TODO:  Add default values for your properties i.e. Name = invalidText
+                Name = invalidText,
+                Country = invalidText
             };
 
             var invalidNationalityException =
@@ -63,11 +64,13 @@ namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Nationalities
                 key: nameof(Nationality.Id),
                 values: "Id is required");
 
-            //invalidNationalityException.AddData(
-            //    key: nameof(Nationality.Name),
-            //    values: "Text is required");
-
-            // TODO: Add or remove data here to suit the validation needs for the Nationality model
+            invalidNationalityException.AddData(
+                key: nameof(Nationality.Name),
+                values: "Text is required");
+            
+            invalidNationalityException.AddData(
+                key: nameof(Nationality.Country),
+                values: "Text is required");
 
             invalidNationalityException.AddData(
                 key: nameof(Nationality.CreatedDate),
