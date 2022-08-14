c84ea55470fce857be787a06ca965937f79c2705
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-14 20:20:49 +0400

    ShouldThrowValidationExceptionOnModifyIfNationalityIsNullAndLogItAsync -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Nationalities/NationalityServiceTests.Validations.Modify.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RefugeeLand.Core.Api.Models.Nationalities;
+using RefugeeLand.Core.Api.Models.Nationalities.Exceptions;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Nationalities
+{
+    public partial class NationalityServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfNationalityIsNullAndLogItAsync()
+        {
+            // given
+            Nationality nullNationality = null;
+            var nullNationalityException = new NullNationalityException();
+
+            var expectedNationalityValidationException =
+                new NationalityValidationException(nullNationalityException);
+
+            // when
+            ValueTask<Nationality> modifyNationalityTask =
+                this.nationalityService.ModifyNationalityAsync(nullNationality);
+
+            NationalityValidationException actualNationalityValidationException =
+                await Assert.ThrowsAsync<NationalityValidationException>(
+                    modifyNationalityTask.AsTask);
+
+            // then
+            actualNationalityValidationException.Should()
+                .BeEquivalentTo(expectedNationalityValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedNationalityValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateNationalityAsync(It.IsAny<Nationality>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
