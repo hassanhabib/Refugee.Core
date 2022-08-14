8c3c426e0df2ba88c09430c11cee549605ecbf14
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-14 20:20:37 +0400

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfNationalityIsNotFoundAndLogItAsync -> FAIL

--- RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Nationalities/NationalityServiceTests.Validations.RetrieveById.cs
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Nationalities/NationalityServiceTests.Validations.RetrieveById.cs
@@ -51,5 +51,47 @@ namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Nationalities
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfNationalityIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someNationalityId = Guid.NewGuid();
+            Nationality noNationality = null;
+
+            var notFoundNationalityException =
+                new NotFoundNationalityException(someNationalityId);
+
+            var expectedNationalityValidationException =
+                new NationalityValidationException(notFoundNationalityException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectNationalityByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noNationality);
+
+            //when
+            ValueTask<Nationality> retrieveNationalityByIdTask =
+                this.nationalityService.RetrieveNationalityByIdAsync(someNationalityId);
+
+            NationalityValidationException actualNationalityValidationException =
+                await Assert.ThrowsAsync<NationalityValidationException>(
+                    retrieveNationalityByIdTask.AsTask);
+
+            //then
+            actualNationalityValidationException.Should().BeEquivalentTo(expectedNationalityValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectNationalityByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedNationalityValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
