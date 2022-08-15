aab0b33004557d438ba06c88481f7ac6109093eb
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-08-15 17:47:52 +0400

    CODE RUB: Fix Case

--- RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
@@ -16,7 +16,7 @@ namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.Hosts
     public partial class HostServiceTests
     {
         [Fact]
-        public async Task shouldThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync()
+        public async Task ShouldThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync()
         {
             // given
             Host nullHost = null;
