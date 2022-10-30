a400697d4c3062bde83bf9e4bb23e72b6c7f75ac
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-30 23:25:54 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/ShelterOffers/ShelterOfferServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using RefugeeLand.Core.Api.Models.ShelterOffers;
+using RefugeeLand.Core.Api.Models.ShelterOffers.Exceptions;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.ShelterOffers
+{
+    public partial class ShelterOfferServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedShelterOfferStorageException =
+                new FailedShelterOfferStorageException(sqlException);
+
+            var expectedShelterOfferDependencyException =
+                new ShelterOfferDependencyException(failedShelterOfferStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectShelterOfferByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<ShelterOffer> retrieveShelterOfferByIdTask =
+                this.shelterOfferService.RetrieveShelterOfferByIdAsync(someId);
+
+            ShelterOfferDependencyException actualShelterOfferDependencyException =
+                await Assert.ThrowsAsync<ShelterOfferDependencyException>(
+                    retrieveShelterOfferByIdTask.AsTask);
+
+            // then
+            actualShelterOfferDependencyException.Should()
+                .BeEquivalentTo(expectedShelterOfferDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectShelterOfferByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedShelterOfferDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
