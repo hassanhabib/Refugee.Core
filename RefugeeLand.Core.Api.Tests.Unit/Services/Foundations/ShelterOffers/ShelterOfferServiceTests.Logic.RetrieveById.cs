fbae73d384e8c83fa45fb9c8fe7e5f232f105100
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-30 23:25:52 +0100

    ShouldRetrieveShelterOfferByIdAsync -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/ShelterOffers/ShelterOfferServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using RefugeeLand.Core.Api.Models.ShelterOffers;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.ShelterOffers
+{
+    public partial class ShelterOfferServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveShelterOfferByIdAsync()
+        {
+            // given
+            ShelterOffer randomShelterOffer = CreateRandomShelterOffer();
+            ShelterOffer inputShelterOffer = randomShelterOffer;
+            ShelterOffer storageShelterOffer = randomShelterOffer;
+            ShelterOffer expectedShelterOffer = storageShelterOffer.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectShelterOfferByIdAsync(inputShelterOffer.Id))
+                    .ReturnsAsync(storageShelterOffer);
+
+            // when
+            ShelterOffer actualShelterOffer =
+                await this.shelterOfferService.RetrieveShelterOfferByIdAsync(inputShelterOffer.Id);
+
+            // then
+            actualShelterOffer.Should().BeEquivalentTo(expectedShelterOffer);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectShelterOfferByIdAsync(inputShelterOffer.Id),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
