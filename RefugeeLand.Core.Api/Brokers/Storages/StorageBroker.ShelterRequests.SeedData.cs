d13efc95d317eedf297e5524ec341f9cbee4f2b4
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-11-15 22:42:17 +0100

    CODE RUB: Add Copyrights

--- RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.ShelterRequests.SeedData.cs
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.ShelterRequests.SeedData.cs
@@ -1,3 +1,8 @@
+// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
 using Microsoft.EntityFrameworkCore;
 using RefugeeLand.Core.Api.Models.ShelterRequests;
 
