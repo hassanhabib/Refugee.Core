1309f7d615d216e17457004d4006c4eee4e19ada
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-11-16 01:09:55 +0200

    CODE RUB: Comment Out ShelterRequest Props Of Primary Entities For Standardly

--- RefugeeLand.Core.Api/Models/ShelterRequests/ShelterRequest.cs
+++ RefugeeLand.Core.Api/Models/ShelterRequests/ShelterRequest.cs
@@ -4,28 +4,25 @@
 // -------------------------------------------------------
 
 using System;
-using RefugeeLand.Core.Api.Models.RefugeeGroups;
-using RefugeeLand.Core.Api.Models.Refugees;
-using RefugeeLand.Core.Api.Models.ShelterOffers;
 
 namespace RefugeeLand.Core.Api.Models.ShelterRequests
 {
     public class ShelterRequest
     {
         public Guid Id { get; set; }
-    
+
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
         public ShelterRequestStatus Status { get; set; }
 
         public Guid ShelterOfferId { get; set; }
-        public ShelterOffer ShelterOffer { get; set; }
+        //    public ShelterOffer ShelterOffer { get; set; }
 
         public Guid RefugeeGroupId { get; set; }
-        public RefugeeGroup RefugeeGroup { get; set; }
+        //    public RefugeeGroup RefugeeGroup { get; set; }
 
         public Guid RefugeeApplicantId { get; set; }
-        public Refugee RefugeeApplicant { get; set; }
+        //   public Refugee RefugeeApplicant { get; set; }
 
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
