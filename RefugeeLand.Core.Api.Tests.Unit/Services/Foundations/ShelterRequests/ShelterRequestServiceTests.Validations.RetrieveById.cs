c72160074c3d79aa6cc59ec27b88c275a238a86b
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-11-16 01:20:09 +0200

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/ShelterRequests/ShelterRequestServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RefugeeLand.Core.Api.Models.ShelterRequests;
+using RefugeeLand.Core.Api.Models.ShelterRequests.Exceptions;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.ShelterRequests
+{
+    public partial class ShelterRequestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidShelterRequestId = Guid.Empty;
+
+            var invalidShelterRequestException =
+                new InvalidShelterRequestException();
+
+            invalidShelterRequestException.AddData(
+                key: nameof(ShelterRequest.Id),
+                values: "Id is required");
+
+            var expectedShelterRequestValidationException =
+                new ShelterRequestValidationException(invalidShelterRequestException);
+
+            // when
+            ValueTask<ShelterRequest> retrieveShelterRequestByIdTask =
+                this.shelterRequestService.RetrieveShelterRequestByIdAsync(invalidShelterRequestId);
+
+            ShelterRequestValidationException actualShelterRequestValidationException =
+                await Assert.ThrowsAsync<ShelterRequestValidationException>(
+                    retrieveShelterRequestByIdTask.AsTask);
+
+            // then
+            actualShelterRequestValidationException.Should()
+                .BeEquivalentTo(expectedShelterRequestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedShelterRequestValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectShelterRequestByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
