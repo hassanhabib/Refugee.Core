329da09b4a2b2f9bf674bea9408a281519261e8f
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-11-16 01:20:33 +0200

    ShouldThrowValidationExceptionOnModifyIfShelterRequestIsInvalidAndLogItAsync -> FAIL

--- RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/ShelterRequests/ShelterRequestServiceTests.Validations.Modify.cs
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/ShelterRequests/ShelterRequestServiceTests.Validations.Modify.cs
@@ -48,5 +48,73 @@ namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.ShelterRequests
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfShelterRequestIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidShelterRequest = new ShelterRequest
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidShelterRequestException = new InvalidShelterRequestException();
+
+            invalidShelterRequestException.AddData(
+                key: nameof(ShelterRequest.Id),
+                values: "Id is required");
+
+            //invalidShelterRequestException.AddData(
+            //    key: nameof(ShelterRequest.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the ShelterRequest model
+
+            invalidShelterRequestException.AddData(
+                key: nameof(ShelterRequest.CreatedDate),
+                values: "Date is required");
+
+            invalidShelterRequestException.AddData(
+                key: nameof(ShelterRequest.CreatedByUserId),
+                values: "Id is required");
+
+            invalidShelterRequestException.AddData(
+                key: nameof(ShelterRequest.UpdatedDate),
+                values: "Date is required");
+
+            invalidShelterRequestException.AddData(
+                key: nameof(ShelterRequest.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedShelterRequestValidationException =
+                new ShelterRequestValidationException(invalidShelterRequestException);
+
+            // when
+            ValueTask<ShelterRequest> modifyShelterRequestTask =
+                this.shelterRequestService.ModifyShelterRequestAsync(invalidShelterRequest);
+
+            ShelterRequestValidationException actualShelterRequestValidationException =
+                await Assert.ThrowsAsync<ShelterRequestValidationException>(
+                    modifyShelterRequestTask.AsTask);
+
+            //then
+            actualShelterRequestValidationException.Should().BeEquivalentTo(expectedShelterRequestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedShelterRequestValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateShelterRequestAsync(It.IsAny<ShelterRequest>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
