890ba14ffe8cb355492d13fac4fda8d1ecb71596
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-11-29 13:42:07 +0100

    BROKERS: Storage - Insert Pet

---
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Pets.Configurations.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RefugeeLand.Core.Api.Models.Pets;
+
+namespace RefugeeLand.Core.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        private static void AddPetConfigurations(ModelBuilder modelBuilder)
+        {
+            // TODO: Add your fluent configurations here
+            // TODO: Call this method from StorageBroker.cs AddConfigurations method
+        }
+    }
+}
