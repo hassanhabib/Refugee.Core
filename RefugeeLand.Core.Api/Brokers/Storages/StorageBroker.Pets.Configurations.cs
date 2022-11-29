b2e3b36e43ac811d0cf3f7a2103f4b6ea1bab661
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-11-29 13:53:04 +0100

    CODE RUB: Cleaning AddPetConfigurations

--- RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Pets.Configurations.cs
+++ RefugeeLand.Core.Api/Brokers/Storages/StorageBroker.Pets.Configurations.cs
@@ -1,3 +1,8 @@
+// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
 using Microsoft.EntityFrameworkCore;
 using RefugeeLand.Core.Api.Models.Pets;
 
@@ -6,9 +11,6 @@ namespace RefugeeLand.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         private static void AddPetConfigurations(ModelBuilder modelBuilder)
-        {
-            // TODO: Add your fluent configurations here
-            // TODO: Call this method from StorageBroker.cs AddConfigurations method
-        }
+        { }
     }
 }
