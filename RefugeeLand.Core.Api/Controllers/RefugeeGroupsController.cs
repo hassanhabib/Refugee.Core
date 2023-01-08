d82c360895e3bad96ef86710fd7a569588d87a92
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2023-01-08 14:32:53 +0100

    CONTROLLERS: POST RefugeeGroup

---
+++ RefugeeLand.Core.Api/Controllers/RefugeeGroupsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using RefugeeLand.Core.Api.Models.RefugeeGroups;
+using RefugeeLand.Core.Api.Models.RefugeeGroups.Exceptions;
+using RefugeeLand.Core.Api.Services.Foundations.RefugeeGroups;
+
+namespace RefugeeLand.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RefugeeGroupsController : RESTFulController
+    {
+        private readonly IRefugeeGroupService refugeeGroupService;
+
+        public RefugeeGroupsController(IRefugeeGroupService refugeeGroupService) =>
+            this.refugeeGroupService = refugeeGroupService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<RefugeeGroup>> PostRefugeeGroupAsync(RefugeeGroup refugeeGroup)
+        {
+            try
+            {
+                RefugeeGroup addedRefugeeGroup =
+                    await this.refugeeGroupService.AddRefugeeGroupAsync(refugeeGroup);
+
+                return Created(addedRefugeeGroup);
+            }
+            catch (RefugeeGroupValidationException refugeeGroupValidationException)
+            {
+                return BadRequest(refugeeGroupValidationException.InnerException);
+            }
+            catch (RefugeeGroupDependencyValidationException refugeeGroupValidationException)
+                when (refugeeGroupValidationException.InnerException is InvalidRefugeeGroupReferenceException)
+            {
+                return FailedDependency(refugeeGroupValidationException.InnerException);
+            }
+            catch (RefugeeGroupDependencyValidationException refugeeGroupDependencyValidationException)
+               when (refugeeGroupDependencyValidationException.InnerException is AlreadyExistsRefugeeGroupException)
+            {
+                return Conflict(refugeeGroupDependencyValidationException.InnerException);
+            }
+            catch (RefugeeGroupDependencyException refugeeGroupDependencyException)
+            {
+                return InternalServerError(refugeeGroupDependencyException);
+            }
+            catch (RefugeeGroupServiceException refugeeGroupServiceException)
+            {
+                return InternalServerError(refugeeGroupServiceException);
+            }
+        }
+    }
+}
