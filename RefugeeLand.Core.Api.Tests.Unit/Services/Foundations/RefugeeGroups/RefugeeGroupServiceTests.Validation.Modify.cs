48b654997d57c6739d2b78b80549a435619c1a1b
spectralgo <florian.renard@spectralgo.com>
2023-03-17 22:46:43 +0100

    ShouldThrowValidationExceptionOnModifyIfRefugeeGroupIsNullAndLogItAsync -> FAIL

---
+++ RefugeeLand.Core.Api.Tests.Unit/Services/Foundations/RefugeeGroups/RefugeeGroupServiceTests.Validation.Modify.cs
@@ -0,0 +1,60 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO DELIVER HUMANITARIAN AID, HOPE AND LOVE
+// -------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using RefugeeLand.Core.Api.Models.RefugeeGroups;
+using RefugeeLand.Core.Api.Models.RefugeeGroups.Exceptions;
+using Xunit;
+
+namespace RefugeeLand.Core.Api.Tests.Unit.Services.Foundations.RefugeeGroups
+{
+    public partial class RefugeeGroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfRefugeeGroupIsNullAndLogItAsync()
+        {
+            // given
+            RefugeeGroup nullRefugeeGroup = null;
+            var nullRefugeeGroupException = new NullRefugeeGroupException();
+
+            var expectedRefugeeGroupValidationException =
+                new RefugeeGroupValidationException(nullRefugeeGroupException);
+
+            // when
+            ValueTask<RefugeeGroup> modifyRefugeeGroupTask =
+                this.refugeeGroupService.ModifyRefugeeGroupAsync(nullRefugeeGroup);
+
+            RefugeeGroupValidationException actualRefugeeGroupValidationException =
+                await Assert.ThrowsAsync<RefugeeGroupValidationException>(
+                    modifyRefugeeGroupTask.AsTask);
+
+            // then
+            actualRefugeeGroupValidationException.Should()
+                .BeEquivalentTo(expectedRefugeeGroupValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedRefugeeGroupValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateRefugeeGroupAsync(It.IsAny<RefugeeGroup>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
